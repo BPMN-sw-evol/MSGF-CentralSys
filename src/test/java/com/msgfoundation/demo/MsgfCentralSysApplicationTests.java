79f88c4cca89d5fa0cde9fd58c741359c7c5aad2
danilonunezgil <danilo.gil98@gmail.com>
2023-09-29 15:38:51 -0500

    first code central sys

---
+++ src/test/java/com/msgfoundation/demo/MsgfCentralSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msgfoundation.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgfCentralSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
