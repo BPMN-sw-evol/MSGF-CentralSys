854111bbdc94afe0f12390af5c546e5aac1d3d00
danilonunezgil <danilo.gil98@gmail.com>
2023-11-08 13:19:51 -0500

    separate methods and services code by credit analyst

--- src/main/java/com/MSGFCentralSys/MSGFCentralSys/controller/CreditAnalystController.java
+++ src/main/java/com/MSGFCentralSys/MSGFCentralSys/controller/CreditAnalystController.java
@@ -2,7 +2,8 @@ package com.MSGFCentralSys.MSGFCentralSys.controller;
 
 import com.MSGFCentralSys.MSGFCentralSys.dto.CreditRequestDTO;
 import com.MSGFCentralSys.MSGFCentralSys.dto.TaskInfo;
-import com.MSGFCentralSys.MSGFCentralSys.services.CreditAnalystServices;
+import com.MSGFCentralSys.MSGFCentralSys.services.CreditAnalystCoupleServices;
+import com.MSGFCentralSys.MSGFCentralSys.services.CreditAnalystValidateService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,23 +16,26 @@ import java.util.List;
 @Controller
 public class CreditAnalystController {
 
-    private final CreditAnalystServices camundaService;
+    private final CreditAnalystCoupleServices creditAnalystCoupleServices;
+    private final CreditAnalystValidateService creditAnalystValidateService;
     List<CreditRequestDTO> processVariablesListCA = new ArrayList<>();
 
-    public CreditAnalystController(CreditAnalystServices camundaService) {
-        this.camundaService = camundaService;
+    public CreditAnalystController(CreditAnalystCoupleServices creditAnalystCoupleServices, CreditAnalystValidateService creditAnalystValidateService) {
+        this.creditAnalystCoupleServices = creditAnalystCoupleServices;
+        this.creditAnalystValidateService = creditAnalystValidateService;
     }
 
 
     @GetMapping("/credit-analyst-couple")
     public String CreditAnalystCoupleView(Model model) throws IOException {
 
-        List<String> processIds = this.camundaService.getAllProcessByCreditAnalyst("CreditAnalyst");
+        List<String> processIds = this.creditAnalystCoupleServices.getAllProcessByActivityId("Activity_0h13zv2");
+        System.out.println(processIds.toString());
         processVariablesListCA.clear();
         // Iterar a través de los processIds y obtener las variables para cada uno
         for (String processId : processIds) {
-            CreditRequestDTO creditRequestDTO = this.camundaService.getProcessVariablesById(processId);
-            TaskInfo taskInfo = this.camundaService.getTaskInfoByProcessId(processId);
+            CreditRequestDTO creditRequestDTO = this.creditAnalystCoupleServices.getProcessVariablesById(processId);
+            TaskInfo taskInfo = this.creditAnalystCoupleServices.getTaskInfoByProcessId(processId);
             creditRequestDTO.setTaskInfo(taskInfo);
             processVariablesListCA.add(creditRequestDTO);
         }
@@ -46,12 +50,13 @@ public class CreditAnalystController {
     @GetMapping("/credit-analyst-validate")
     public String CreditAnalystValidateView(Model model) throws IOException {
 
-        List<String> processIds = this.camundaService.getAllProcessByCreditAnalyst("CreditAnalyst");
+        List<String> processIds = this.creditAnalystValidateService.getAllProcessByActivityId("Activity_0w7pg72");
+        System.out.println(processIds.toString());
         processVariablesListCA.clear();
         // Iterar a través de los processIds y obtener las variables para cada uno
         for (String processId : processIds) {
-            CreditRequestDTO creditRequestDTO = this.camundaService.getProcessVariablesById(processId);
-            TaskInfo taskInfo = this.camundaService.getTaskInfoByProcessId(processId);
+            CreditRequestDTO creditRequestDTO = this.creditAnalystValidateService.getProcessVariablesById(processId);
+            TaskInfo taskInfo = this.creditAnalystValidateService.getTaskInfoByProcessId(processId);
             creditRequestDTO.setTaskInfo(taskInfo);
             processVariablesListCA.add(creditRequestDTO);
         }
@@ -64,11 +69,20 @@ public class CreditAnalystController {
     }
 
     @GetMapping("/complete-credit-analyst-couple")
-    public String completeTaskCreditAnalyst(@RequestParam(name = "taskId") String taskId, @RequestParam(name="assignee") String assignee, @RequestParam(name="value") Boolean value, @RequestParam("variable") String variable) throws IOException {
+    public String completeTaskCreditAnalystCouple(@RequestParam(name = "taskId") String taskId, @RequestParam(name="assignee") String assignee, @RequestParam(name="value") Boolean value, @RequestParam("variable") String variable) throws IOException {
         System.out.println("aqui estoy"+value);
         System.out.println("aqui estoy"+assignee);
 
-        this.camundaService.completeTask(taskId, assignee, value, variable);
+        this.creditAnalystCoupleServices.completeTask(taskId, assignee, value, variable);
+        return "redirect:/credit-analyst-couple";
+    }
+
+    @GetMapping("/complete-credit-analyst-validate")
+    public String completeTaskCreditAnalystValidate(@RequestParam(name = "taskId") String taskId, @RequestParam(name="assignee") String assignee, @RequestParam(name="value") Boolean value, @RequestParam("variable") String variable) throws IOException {
+        System.out.println("aqui estoy"+value);
+        System.out.println("aqui estoy"+assignee);
+
+        this.creditAnalystValidateService.completeTask(taskId, assignee, value, variable);
         return "redirect:/credit-analyst-couple";
     }
 }
